93fa754e30741fc08d08aa2c4491bce8cca2923b
Sujay Jeppu <sujayrj@gmail.com>
2024-05-30 08:00:10 +0530

    Base project

---
+++ src/main/java/dev/jeppu/HogwartsArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package dev.jeppu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactsOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsArtifactsOnlineApplication.class, args);
+    }
+
+}
